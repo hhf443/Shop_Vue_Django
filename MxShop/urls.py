b250beb30b89d9ba70880abbac18bc3fab9fa2ed
hhf443 <1361704345@qq.com>
2020-01-12 16:28:26 +0800

    urls.py修复图片不能显示bug

--- MxShop/urls.py
+++ MxShop/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.static import serve
+
+from MxShop.settings import MEDIA_ROOT
+
 import xadmin
 urlpatterns = [
        path('xadmin/', xadmin.site.urls),
+       # path('media/(?P<path>.*)$', serve, {"document_root":MEDIA_ROOT}),
+       re_path(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),
 ]
